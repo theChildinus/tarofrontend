831008076345cbab5827715a71a398a23ab5b9ae
theChildinus <zky0825@163.com>
2019-10-18 15:55:19 +0800

    add Edit User Role

--- src/components/page/User/AddUser.vue
+++ src/components/page/User/AddUser.vue
@@ -15,11 +15,9 @@
                         <el-input v-model="form.user_name"></el-input>
                     </el-form-item>
                     <el-form-item label="身份">
-                        <el-radio-group v-model="form.user_role">
-                            <el-radio label="管理员"></el-radio>
-                            <el-radio label="员工"></el-radio>
-                            <el-radio label="经理"></el-radio>
-                        </el-radio-group>
+                        <el-select v-model="form.type" placeholder="用户身份" class="handle-select mr10">
+                            <el-option v-for="item in enumValueList" :key="item" :label="item" :value="item"></el-option>
+                        </el-select>
                     </el-form-item>
                     <el-form-item label="部门">
                         <el-input v-model="form.user_department"></el-input>
@@ -35,6 +33,7 @@
                     </el-form-item>
                     <el-form-item>
                         <el-button type="primary" @click="onSubmit">表单提交</el-button>
+                        <el-button type="danger" @click="onClear">清空</el-button>
                         <el-button>取消</el-button>
                     </el-form-item>
                 </el-form>
@@ -51,8 +50,12 @@ export default {
             form: {
                 name: '',
                 type: ''
-            }
+            },
+            enumValueList: []
         };
+    },
+    created() {
+        this.getEnumData();
     },
     methods: {
         onSubmit() {
@@ -68,6 +71,31 @@ export default {
             .then( (res) => {
                 this.$message.success('提交成功！');    
             })
+        },
+        onClear() {
+            this.form = {};
+        },
+        // 获取用户角色数据
+        getEnumData() {
+            this.$axios
+                .post('api/enum/getValue', {
+                    enum_key: 'user_role'
+                })
+                .then(res => {
+                    // console.log('resdata: ', res.data);
+                    this.parseEnumData(res.data.enum_value);
+                });
+        },
+        // 处理用户角色数据
+        parseEnumData(data) {
+            var str = data.split('##');
+            for (var i = 0; i < str.length; i++) {
+                if (str[i] == '' || typeof str[i] == 'undefined') {
+                    str.splice(i, 1);
+                    i = i - 1;
+                }
+            }
+            this.enumValueList = str;
         }
     }
 };
--- src/components/page/User/UserTable.vue
+++ src/components/page/User/UserTable.vue
@@ -9,14 +9,17 @@
         </div>
         <div class="container">
             <div class="handle-box">
-                <el-button
+                <!-- <el-button
                     type="primary"
                     icon="el-icon-delete"
                     class="handle-del mr10"
                     @click="delAllSelection"
-                >批量删除</el-button>
+                >批量删除</el-button> -->
                 <el-input v-model="search.searchName" placeholder="用户名" class="handle-input mr10"></el-input>
                 <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+                <el-button type="primary" icon="el-icon-refresh" @click="handleRefresh">刷新</el-button>
+                <el-button type="danger" icon="el-icon-delete" @click="clearSelection">清空</el-button>
+                <el-button type="primary" icon="el-icon-edit" @click="handleEnumEdit">编辑用户身份</el-button>
             </div>
             <el-table
                 :data="tableData"
@@ -82,11 +85,13 @@
                     <el-input v-model="form.user_name"></el-input>
                 </el-form-item>
                 <el-form-item label="身份">
-                        <el-radio-group v-model="form.user_role">
-                            <el-radio label="管理员"></el-radio>
-                            <el-radio label="员工"></el-radio>
-                            <el-radio label="经理"></el-radio>
-                        </el-radio-group>
+                    <el-select
+                        v-model="form.user_role"
+                        placeholder="用户身份"
+                        class="handle-select mr10"
+                    >
+                        <el-option v-for="item in enumValueList" :key="item" :label="item" :value="item"></el-option>
+                    </el-select>
                     </el-form-item>
                     <el-form-item label="部门">
                         <el-input v-model="form.user_department"></el-input>
@@ -104,6 +109,15 @@
             <span slot="footer" class="dialog-footer">
                 <el-button @click="editVisible = false">取 消</el-button>
                 <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 编辑用户身份弹出框 -->
+        <el-dialog title="编辑用户身份（以 ## 分隔）" :visible.sync="editEnumVisible" width="30%">
+            <el-input type="textarea" autosize placeholder="请输入内容" v-model="enumObj.enum_value"></el-input>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editEnumVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEnumEdit">保 存</el-button>
             </span>
         </el-dialog>
     </div>
@@ -124,6 +138,9 @@ export default {
             multipleSelection: [],
             delList: [],
             editVisible: false,
+            enumObj: {},
+            enumValueList: [],
+            editEnumVisible: false,
             pageTotal: 0,
             form: {},
             idx: -1,
@@ -132,6 +149,7 @@ export default {
     },
     created() {
         this.getData();
+        this.getEnumData();
     },
     methods: {
         // 获取 easy-mock 的模拟数据
@@ -151,6 +169,12 @@ export default {
                     this.tableData = res.data.list;
                     this.pageTotal = res.data.count || 50;
                 });
+        },
+        handleRefresh() {
+            this.getData();
+        },
+        clearSelection() {
+            this.search = {};
         },
         // 触发搜索按钮
         handleSearch() {
@@ -234,8 +258,46 @@ export default {
                     }
                 });
         },
-        handleEditRole() {
-            console.log("EditRole")
+        // 获取策略动作数据
+        getEnumData() {
+            this.$axios
+                .post('api/enum/getValue', {
+                    enum_key: 'user_role'
+                })
+                .then(res => {
+                    // console.log('resdata: ', res.data);
+                    this.enumObj = res.data;
+                    this.parseEnumData(this.enumObj.enum_value);
+                });
+        },
+        // 触发策略动作编辑
+        handleEnumEdit() {
+            this.editEnumVisible = true;
+        },
+        // 保存策略动作编辑
+        saveEnumEdit() {
+            this.editEnumVisible = false;
+            this.$axios
+                .post('api/enum/putValue', {
+                    enum_id: this.enumObj.enum_id,
+                    enum_key: this.enumObj.enum_key,
+                    enum_value: this.enumObj.enum_value
+                })
+                .then(res => {
+                    this.parseEnumData(this.enumObj.enum_value);
+                    this.$message.success(`保存成功`);
+                });
+        },
+        // 处理策略动作数据
+        parseEnumData(data) {
+            var str = data.split('##');
+            for (var i = 0; i < str.length; i++) {
+                if (str[i] == '' || typeof str[i] == 'undefined') {
+                    str.splice(i, 1);
+                    i = i - 1;
+                }
+            }
+            this.enumValueList = str;
         }
     }
 };
