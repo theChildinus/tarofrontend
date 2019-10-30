116f0c489eca0e283a3357c0ad0958e9b1e5d052
theChildinus <zky0825@163.com>
2019-10-30 16:51:45 +0800

    dev mod conf

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,9 @@ module.exports = {
     assetsDir: 'static',
     productionSourceMap: false,
     devServer: {
-        https: true,
         proxy: {
             '/api':{
-                target: 'https://127.0.0.1:4433',
+                target: 'http://127.0.0.1:9090',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
